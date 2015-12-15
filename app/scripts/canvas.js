0693c3d702cde0db379855de49d3d8288cc52af0
Ross <rdegnen@gmail.com>
2015-12-15 14:16:54 -0500

    slider with rasters is working

--- app/scripts/canvas.js
+++ app/scripts/canvas.js
@@ -1,3 +1,4 @@
+// First img that is loaded
 var raster = new Raster('slider-active'),
 loaded = false;
 
@@ -56,6 +57,7 @@ $(document).ready(function() {
   current = 0;
 
   $('.btn-slider').on('click', function() {
+
     var img = slides.eq(current),
     nextIndex = 0;
 
@@ -71,7 +73,57 @@ $(document).ready(function() {
     next.attr('id', 'slider-active');
     img.attr('id', '');
 
+    // Functions called again for new images when loaded on the click event
     var raster = new Raster('slider-active');
-    raster.position = view.center;
-  });
-});
+    loaded = false;
+
+    raster.on('load', function() {
+      loaded = true;
+      onResizeNew();
+    });
+
+    raster.visible = false;
+
+    var lastPos = view.center;
+    function moveHandlerNew(event) {
+      if (!loaded) {return;}
+      if (lastPos.getDistance(event.point) < 10) {return;}
+
+      lastPos = event.point;
+
+      var size = this.bounds.size.clone();
+      var isLandscape = size.width > size.height;
+
+      size /= isLandscape ? [2,1] : [1,2];
+
+      var path = new Path.Rectangle({
+        point: this.bounds.topLeft.floor(),
+        size: size.ceil(),
+        onMouseMove: moveHandlerNew
+      });
+      path.fillColor = raster.getAverageColor(path);
+
+      var path = new Path.Rectangle({
+        point: isLandscape ? this.bounds.topCenter.ceil() : this.bounds.leftCenter.ceil(),
+        size: size.floor(),
+        onMouseMove: moveHandlerNew
+      });
+      path.fillColor = raster.getAverageColor(path);
+
+      this.remove();
+    }
+
+    function onResizeNew(event) {
+      if (!loaded) {return;}
+
+      project.activeLayer.removeChildren();
+      raster.fitBounds(view.bounds, true);
+
+      new Path.Rectangle({
+        rectangle: view.bounds,
+        fillColor: raster.getAverageColor(view.bounds),
+        onMouseMove: moveHandlerNew
+      });
+    }
+  }); // on click
+}); // document ready
